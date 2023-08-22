fa6dcfa45646e5d937963976a5b5060b9e4afaae
gabriellmeireles <gabriellmeireles@hotmail.com>
2023-08-21 21:51:11 -0300

    Criado a rota 'clients'; Implementado no ClientController o metodo 'index'; Alerado o relacionamento no model Client para belongsTo; Criado a view 'index' do Client.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,7 +13,9 @@ class ClientController extends Controller
      */
     public function index()
     {
-        return "Beer & Code";
+        $clients = Client::with('user','address')->paginate(10);
+
+        return view('clients.index', compact('clients'));
     }
 
     /**
@@ -21,7 +23,7 @@ class ClientController extends Controller
      */
     public function create()
     {
-        //
+        return view('clients.create');
     }
 
     /**
